8e64424c328529c9c5daba38428ecf66f927f22a
wild-mental <wild.mental@kidsnote.com>
2024-04-19 11:54:10 +0900

    initial commit 2

---
+++ src/main/java/ac/su/ajaxspringinclass/AjaxSpringInClassApplication.java
@@ -0,0 +1,13 @@
+package ac.su.ajaxspringinclass;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AjaxSpringInClassApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(AjaxSpringInClassApplication.class, args);
+    }
+
+}
---
+++ src/test/java/ac/su/ajaxspringinclass/AjaxSpringInClassApplicationTests.java
@@ -0,0 +1,13 @@
+package ac.su.ajaxspringinclass;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AjaxSpringInClassApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
